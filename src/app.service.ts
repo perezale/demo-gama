984739efea2b9b87f2292b2e47218979ce9369b6
ealcca <edual986@gmail.com>
2022-09-30 15:45:03 -0300

    segund test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Carlos Eduardo!';
+    return 'Hello Carlos Eduardo SEGUNDA PRUEBA!';
   }
 }
