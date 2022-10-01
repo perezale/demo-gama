bd7d4453416a223fbd9c11db3ca833b2d9a67b64
Carlos Alcca <edual986@gmail.com>
2022-10-01 02:16:52 -0300

    initial commit notebook

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Carlos Eduardo SEGUNDA PRUEBA!';
+    return 'Hello Carlos en la notebook';
   }
 }
