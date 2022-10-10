15620d785ade98028acaa1bbc6eba69b1accc470
ealcca <edual986@gmail.com>
2022-10-10 15:19:04 -0300

    add method findOne in module Brand

--- src/brands/brands.controller.ts
+++ src/brands/brands.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BrandsService } from './brands.service';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { Brand } from './interfaces/brand.interface';
@@ -11,6 +11,11 @@ export class BrandsController {
     @Get()
     findAll(){
         return this.brandsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param()params){
+        return this.brandsService.findOne(params.id);
     }
 
     /*
--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -4,17 +4,32 @@ import { Brand } from './interfaces/brand.interface';
 
 @Injectable()
 export class BrandsService {
+    
 
     private brands = [
         {
             "id":"1",
             "nombre":"Toyota"
+        },
+        {
+            "id":"2",
+            "nombre":"Nissan"
+        },
+        {
+            "id":"3",
+            "nombre":"Mitsubishi"
         }
     ];
 
 
     findAll(){
         return this.brands; 
+    }
+
+    findOne(id: any) {
+        return this.brands.find(function(brand){
+            return brand.id == id;
+        });
     }
 
     create(createBrandDto: CreateBrandDto): Brand {
