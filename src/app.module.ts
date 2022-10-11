98d1c87313e11d13dfe497f4e406c7dabf8a932d
ealcca <edual986@gmail.com>
2022-10-11 20:14:13 -0300

    Import module typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
 import { BrandsModule } from './brands/brands.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Car } from './cars/interfaces/car.interface';
+import { Brand } from './brands/interfaces/brand.interface';
 
 @Module({
-  imports: [CarsModule, BrandsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'my_db',
+      username: 'carlos',
+      password: '123456',      
+      entities: [Car,Brand],
+      synchronize: true,
+    }),    
+    CarsModule, BrandsModule],
   controllers: [],
   providers: [],
 })
