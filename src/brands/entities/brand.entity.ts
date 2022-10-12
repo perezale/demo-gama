7a205124c7a7a018f71440512808b45ab126b03a
ealcca <edual986@gmail.com>
2022-10-12 20:43:24 -0300

    brand entity

--- src/brands/entities/brand.entity.ts
+++ src/brands/entities/brand.entity.ts
@@ -1,11 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class Brand {
+
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     nombre: string;
 
+    /*
     constructor(id:number,nombre:string){
         this.id = id;
         this.nombre = nombre;
-    }
+    }*/
     
 }
 
