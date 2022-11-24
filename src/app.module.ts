ffc3acd944bfbb2e1c0bd56dd4843205bc267336
ealcca <edual986@gmail.com>
2022-11-24 18:06:06 -0300

    test connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,11 @@ import { Brand } from './brands/entities/brand.entity';
 require('dotenv').config()
 
 const db_options = {
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'my_db',
-  password: process.env.DB_PASSWORD || '123456',
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USER || 'carlos',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,  
 }
 
 console.log(db_options);
