05dff2a94f19661a9b69fd7c860fd3427f58dc46
ealcca <edual986@gmail.com>
2022-11-24 21:43:24 -0300

    set environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,17 +8,17 @@ import { Brand } from './brands/entities/brand.entity';
 require('dotenv').config()
 
 const db_options = {
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,  
+  username: process.env.DB_USER || 'carlos',
+  password: process.env.DB_PASSWORD || '123456',
+  database: process.env.DB_NAME || 'my_db',  
 }
 
 console.log(db_options);
 
 @Module({
-  imports: [
+  imports: [ 
     TypeOrmModule.forRoot({
       type: 'postgres',
       ...db_options,      
