6f6bad3af941b908d79e5eb69836beb021791acb
ealcca <edual986@gmail.com>
2023-11-16 19:06:14 -0300

    ignore posrgre_data

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,18 @@ import { BrandsModule } from './brands/brands.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Car } from './cars/entities/car.entity';
 import { Brand } from './brands/entities/brand.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ 
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      database: 'my_db',
-      username: 'carlos',
-      password: '123456',      
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      database: process.env.DATABASE_NAME,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,      
       entities: [Car,Brand],
       synchronize: true,
     }),    
