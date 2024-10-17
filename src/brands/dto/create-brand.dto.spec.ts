d1ccf76c9773ad197304f5ad64deff8c2eb2e140
Alejandro Perez <alejandro.perez@scanoss.com>
2024-10-17 20:54:06 -0300

    fix tests

--- src/brands/dto/create-brand.dto.spec.ts
+++ src/brands/dto/create-brand.dto.spec.ts
@@ -3,6 +3,5 @@ import { CreateBrandDto } from './create-brand.dto';
 describe('CreateBrandDto', () => {
   it('should be defined', () => {
     expect(new CreateBrandDto()).toBeDefined();
-    expect(true).toBe(false);
   });
 });
